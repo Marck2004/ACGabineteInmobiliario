52a5c80e8d0f5e9c43159ca345cf09fea2804526
Marck2004 <marcosmaestrocobo@gmail.com>
2025-08-14 12:23:43 +0200

    feat: refactor project

---
+++ src/consts/NavConsts.ts
@@ -0,0 +1,13 @@
+import type { NavConstType } from "@/interfaces/EmployerInterface";
+
+export const inmueblesList: NavConstType[] = [
+	// { title: "Últimos inmuebles", url: "/#inmuebles" },
+	{ title: "Ventas", url: "/inmuebles/ventas" },
+];
+export const seccionesList: NavConstType[] = [
+	{ title: "Blog", url: "/secciones/blog" },
+	{
+		title: "Presentación de servicios",
+		url: "/secciones/presentacion-de-servicios",
+	},
+];
